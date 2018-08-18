0001ef381f5868ea010367587061cee27a6dd90a
DESKTOP-M6C4JDD\Nikita <nandreev@colectivosvip.com>
2018-08-18 21:50:25 +0100

    DBSeeder clean up

--- src/main/java/limestone/DBSeeder.java
+++ src/main/java/limestone/DBSeeder.java
@@ -18,17 +18,17 @@ public class DBSeeder implements CommandLineRunner {
     }
 
     public void run(String... strings) throws Exception {
-        Task task1 = new Task("Task Name 1", "Descrptn", "TODO");
-        Task task2 = new Task("Task Name 2", "Descrptn", "TODO");
-        Task task3 = new Task("Task Name 3", "Descrptn", "TODO");
-
-
-        // clean repository
-        this.taskRepository.deleteAll();
-
-        // add users
-        List<Task> tasksList = Arrays.asList(task1, task2, task3);
-        this.taskRepository.saveAll(tasksList);
+//        Task task1 = new Task("Task Name 1", "Descrptn", "TODO");
+//        Task task2 = new Task("Task Name 2", "Descrptn", "TODO");
+//        Task task3 = new Task("Task Name 3", "Descrptn", "TODO");
+//
+//
+//        // clean repository
+//        this.taskRepository.deleteAll();
+//
+//        // add users
+//        List<Task> tasksList = Arrays.asList(task1, task2, task3);
+//        this.taskRepository.saveAll(tasksList);
     }
 
 
